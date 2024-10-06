b4092ed2ee98289f7d41aa6389a9db08deaa2ef9
shafiq0225 <shafiqahamed.be@gmail.com>
2024-10-06 12:40:34 +0530

    layout created

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { BG_IMG_URL, LOGO_URL } from '../../constants/config';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  logoUrl = LOGO_URL;
+  bgUrl = BG_IMG_URL;
 }
