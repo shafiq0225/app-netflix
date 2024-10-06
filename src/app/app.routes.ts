5134114004a3e04e1a2643e169fe7394936898c6
shafiq0225 <shafiqahamed.be@gmail.com>
2024-10-06 12:03:10 +0530

    route created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { BrowseComponent } from './pages/browse/browse.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "browse",
+        component: BrowseComponent
+    },
+    {
+        path: "",
+        redirectTo: "/login",
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: LoginComponent
+    }
+];
